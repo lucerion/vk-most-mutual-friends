33ec87244b3d6a377b6a6ca5fdd40a9158a20169
lucerion <alexander.skachko@gmail.com>
2019-06-25 03:34:29 +0300

    Change quotes code style to single quote

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,28 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
   ],
-  "parser": 'babel-eslint',
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 9,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+    },
   },
-  "plugins": ["react"],
-  "rules": {
-    "indent": ["error", 2],
-    "quotes": ["error", "single"],
-    "semi": ["error", "always"],
-    "max-len": ["error", { "code": 120 }],
-    "comma-dangle": ["error", {
-        "arrays": "always-multiline",
-        "objects": "always-multiline",
-        "imports": "always-multiline",
-        "exports": "always-multiline",
-        "functions": "always-multiline",
-    }],
-    "no-console": ["error", { "allow": ["warn", "error"] }],
-  }
+  'plugins': ['react'],
+  'rules': {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'max-len': ['error', { 'code': 120 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-console': ['error', { 'allow': ['warn', 'error'] }],
+  },
 };
